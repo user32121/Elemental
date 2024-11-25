d8ba621975446a94d1a2369f55c24900ab3b1f44
32121 <pengjupiter5@gmail.com>
2024-11-24 20:04:02 -0500

    initial commit

---
+++ src/client/java/juniper/elemental/ElementalClient.java
@@ -0,0 +1,12 @@
+package juniper.elemental;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElementalClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as
+		// rendering.
+		Elemental.LOGGER.info(Elemental.MOD_ID + " client init");
+	}
+}
