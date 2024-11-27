bad334844f176bb93ebc0d52ffdcab35a2294536
32121 <pengjupiter5@gmail.com>
2024-11-27 14:09:04 -0500

    make conduit waterloggable not optimal, but makes it not overridable by water

--- src/main/java/juniper/elemental/blocks/ConduitBlock.java
+++ src/main/java/juniper/elemental/blocks/ConduitBlock.java
@@ -5,26 +5,33 @@ import juniper.elemental.blocks.ConduitSignalReactions.ConduitReaction;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Waterloggable;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager.Builder;
 import net.minecraft.state.property.EnumProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
+import net.minecraft.world.tick.ScheduledTickView;
 
-public class ConduitBlock extends TriAxisBlock {
+public class ConduitBlock extends TriAxisBlock implements Waterloggable {
     public static final EnumProperty<ConduitSignal> SIGNAL = EnumProperty.of("signal", ConduitSignal.class);
 
     public ConduitBlock(AbstractBlock.Settings settings) {
         super(settings);
-        this.setDefaultState(getDefaultState().with(SIGNAL, ConduitSignal.OFF));
+        this.setDefaultState(getDefaultState().with(SIGNAL, ConduitSignal.OFF).with(Properties.WATERLOGGED, false));
     }
 
     @Override
     protected void appendProperties(Builder<Block, BlockState> builder) {
         super.appendProperties(builder);
-        builder.add(SIGNAL);
+        builder.add(SIGNAL, Properties.WATERLOGGED);
     }
 
     @Override
@@ -65,5 +72,30 @@ public class ConduitBlock extends TriAxisBlock {
         if (state.get(SIGNAL).is_transient) {
             world.scheduleBlockTick(pos, state.getBlock(), 2);
         }
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
+        boolean bl = fluidState.getFluid() == Fluids.WATER;
+        return super.getPlacementState(ctx).with(Properties.WATERLOGGED, bl);
+    }
+
+    @Override
+    protected BlockState getStateForNeighborUpdate(BlockState state, WorldView world, ScheduledTickView tickView,
+            BlockPos pos, Direction direction, BlockPos neighborPos, BlockState neighborState, Random random) {
+        if (state.get(Properties.WATERLOGGED).booleanValue()) {
+            tickView.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+        return super.getStateForNeighborUpdate(state, world, tickView, pos, direction, neighborPos, neighborState,
+                random);
+    }
+
+    @Override
+    protected FluidState getFluidState(BlockState state) {
+        if (state.get(Properties.WATERLOGGED).booleanValue()) {
+            return Fluids.WATER.getStill(false);
+        }
+        return super.getFluidState(state);
     }
 }
