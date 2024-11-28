4e9f9b034e3fa59056e05f8f3fbd07fa1baefad0
32121 <pengjupiter5@gmail.com>
2024-11-27 19:21:32 -0500

    dust data gen

--- src/main/java/juniper/elemental/data/ElementalModelProvider.java
+++ src/main/java/juniper/elemental/data/ElementalModelProvider.java
@@ -1,6 +1,7 @@
 package juniper.elemental.data;
 
-import juniper.elemental.Elemental;
+import java.util.Optional;
+
 import juniper.elemental.blocks.TriAxisBlock;
 import juniper.elemental.init.ElementalBlocks;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
@@ -8,12 +9,18 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.block.Block;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.BlockStateVariant;
+import net.minecraft.data.client.BlockStateVariantMap;
 import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Model;
+import net.minecraft.data.client.ModelIds;
+import net.minecraft.data.client.Models;
 import net.minecraft.data.client.MultipartBlockStateSupplier;
 import net.minecraft.data.client.TextureKey;
 import net.minecraft.data.client.TextureMap;
 import net.minecraft.data.client.VariantSettings;
+import net.minecraft.data.client.VariantsBlockStateSupplier;
 import net.minecraft.data.client.When;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.Identifier;
 
 public class ElementalModelProvider extends FabricModelProvider {
@@ -23,19 +30,19 @@ public class ElementalModelProvider extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
-        triAxisBlock(blockStateModelGenerator, ElementalBlocks.CLOGGED_CONDUIT, "clogged_conduit");
-        triAxisBlock(blockStateModelGenerator, ElementalBlocks.OVERGROWN_CONDUIT, "overgrown_conduit");
+        registerTriAxisBlock(blockStateModelGenerator, ElementalBlocks.CLOGGED_CONDUIT);
+        registerTriAxisBlock(blockStateModelGenerator, ElementalBlocks.OVERGROWN_CONDUIT);
+        registerSnow(blockStateModelGenerator, ElementalBlocks.DUST);
     }
 
-    private void triAxisBlock(BlockStateModelGenerator bsmg, Block block, String name) {
-        TextureMap textureMap = new TextureMap().put(TextureKey.TEXTURE, formatIdentifier(name, "_core"))
-                .put(TextureKey.SIDE, formatIdentifier(name, "_axis"))
-                .put(TextureKey.END, formatIdentifier(name, "_face"))
-                .put(TextureKey.PARTICLE, formatIdentifier(name, "_axis"));
-        Identifier idCore = ElementalModels.TriAxisBlockCore.upload(formatIdentifier(name, "_core"), textureMap,
+    private void registerTriAxisBlock(BlockStateModelGenerator bsmg, Block block) {
+                TextureMap tm = new TextureMap().put(TextureKey.TEXTURE, ModelIds.getBlockSubModelId(block, "_core"))
+                .put(TextureKey.SIDE, ModelIds.getBlockSubModelId(block, "_axis"))
+                .put(TextureKey.END, ModelIds.getBlockSubModelId(block, "_face"));
+        Identifier idCore = ElementalModels.TriAxisBlockCore.upload(ModelIds.getBlockSubModelId(block, "_core"), tm,
                 bsmg.modelCollector);
-        Identifier idAxis = ElementalModels.TriAxisBlockAxis.upload(formatIdentifier(name, "_axis"), textureMap,
-                bsmg.modelCollector);
+        Identifier idAxis = ElementalModels.TriAxisBlockAxis.upload(ModelIds.getBlockSubModelId(block, "_axis"), tm,
+                        bsmg.modelCollector);
         bsmg.blockStateCollector.accept(MultipartBlockStateSupplier.create(block)
                 .with(BlockStateVariant.create().put(VariantSettings.MODEL, idCore))
                 .with(When.create().set(TriAxisBlock.AXIS_X, true),
@@ -49,11 +56,21 @@ public class ElementalModelProvider extends FabricModelProvider {
         bsmg.registerParentedItemModel(block, idAxis);
     }
 
-    @Override
-    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+    private void registerSnow(BlockStateModelGenerator bsmg, Block block) {
+        TextureMap tm = TextureMap.all(block);
+        Identifier idCubeAll = Models.CUBE_ALL.upload(block, tm, bsmg.modelCollector);
+        bsmg.blockStateCollector.accept(VariantsBlockStateSupplier.create(block).coordinate(BlockStateVariantMap
+                .create(Properties.LAYERS).register(height -> {
+                    Model m = new Model(Optional.of(Identifier.ofVanilla( "block/snow_height" + height * 2)), Optional.empty(),                TextureKey.TEXTURE);
+                    Identifier id = ModelIds.getBlockSubModelId(block, "_height" + height * 2);
+                    id = m.upload(id, tm, bsmg.modelCollector);
+                    return BlockStateVariant.create().put(VariantSettings.MODEL,
+                            height < 8 ? id : idCubeAll);
+                })));
+        bsmg.registerParentedItemModel(block, ModelIds.getBlockSubModelId(block, "_height2"));
     }
 
-    private Identifier formatIdentifier(String name, String suffix) {
-        return Identifier.of(Elemental.MOD_ID, "block/" + name + suffix);
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
     }
 }
