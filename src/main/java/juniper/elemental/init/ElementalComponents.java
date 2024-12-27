147ea92e7d2df09d4074cbefd71971e36656e22d
jupiter <pengjupiter5@gmail.com>
2024-12-26 19:58:07 -0600

    remove cooldown component

--- src/main/java/juniper/elemental/init/ElementalComponents.java
+++ src/main/java/juniper/elemental/init/ElementalComponents.java
@@ -2,18 +2,13 @@ package juniper.elemental.init;
 
 import java.util.function.UnaryOperator;
 
-import com.mojang.serialization.Codec;
-
 import juniper.elemental.Elemental;
 import net.minecraft.component.ComponentType;
-import net.minecraft.network.codec.PacketCodecs;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ElementalComponents {
-    public static final ComponentType<Long> LAST_PING_TIME = register("last_ping_time", builder -> builder.codec(Codec.LONG).packetCodec(PacketCodecs.LONG));
-
     public static void init() {
     }
 
--- src/main/java/juniper/elemental/items/RadarItem.java
+++ src/main/java/juniper/elemental/items/RadarItem.java
@@ -1,7 +1,6 @@
 package juniper.elemental.items;
 
 import juniper.elemental.Elemental;
-import juniper.elemental.init.ElementalComponents;
 import juniper.elemental.init.ElementalSounds;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -13,7 +12,6 @@ import net.minecraft.world.World;
 
 public class RadarItem extends Item {
     public final int MAX_COOLDOWN = 5 * 20;
-    public final String COOLDOWN_KEY = "Cooldown";
 
     public RadarItem(Settings settings) {
         super(settings);
@@ -31,13 +29,9 @@ public class RadarItem extends Item {
         if (!world.getDimensionEntry().matchesId(Identifier.of(Elemental.MOD_ID, "dark"))) {
             return;
         }
-        if (world.getTime() - stack.getOrDefault(ElementalComponents.LAST_PING_TIME, 0L) < MAX_COOLDOWN) {
-            return;
-        }
         if (entity instanceof PlayerEntity pe && pe.getItemCooldownManager().getCooldownProgress(stack, 0) > 0) {
             return;
         }
-        stack.set(ElementalComponents.LAST_PING_TIME, world.getTime());
         world.playSound(null, entity.getBlockPos(), ElementalSounds.RADAR, SoundCategory.BLOCKS, 1, 1);
         if (entity instanceof PlayerEntity pe) {
             pe.getItemCooldownManager().set(stack, MAX_COOLDOWN);
