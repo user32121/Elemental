47c138a64ea3ecd66316e2f592be77fb9a05a1dc
jupiter <pengjupiter5@gmail.com>
2024-12-29 21:03:05 -0600

    crystals emit light

--- src/main/java/juniper/elemental/init/ElementalBlocks.java
+++ src/main/java/juniper/elemental/init/ElementalBlocks.java
@@ -45,7 +45,7 @@ public class ElementalBlocks {
     public static Block DARK_BLOCK = register("dark_block", Block::new, AbstractBlock.Settings.create().strength(1.5f).requiresTool().dropsNothing(), true);
     public static Block DARK_PORTAL = register("dark_portal", DarkPortalBlock::new,
             AbstractBlock.Settings.create().noCollision().strength(-1.0f, 3600000.0f).dropsNothing().pistonBehavior(PistonBehavior.BLOCK), false);
-    public static Block LIGHT_CRYSTAL = register("light_crystal", LightCrystalBlock::new, AbstractBlock.Settings.create().strength(1.5f).requiresTool(), true);
+    public static Block LIGHT_CRYSTAL = register("light_crystal", LightCrystalBlock::new, AbstractBlock.Settings.create().strength(1.5f).requiresTool().luminance(state -> 15), true);
 
     public static void init() {
     }
