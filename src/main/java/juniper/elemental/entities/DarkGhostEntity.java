f8c0a3b629d68cd330872bc0ea4e55bc8fc5bc33
jupiter <pengjupiter5@gmail.com>
2025-01-01 11:26:31 -0800

    change dark ghost to use goals system

--- src/main/java/juniper/elemental/entities/DarkGhostEntity.java
+++ src/main/java/juniper/elemental/entities/DarkGhostEntity.java
@@ -1,9 +1,6 @@
 package juniper.elemental.entities;
 
-import java.util.List;
-
-import juniper.elemental.init.ElementalDimensions;
-import juniper.elemental.init.ElementalItems;
+import juniper.elemental.entities.goals.DisappearGoal;
 import net.minecraft.entity.EntityData;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
@@ -11,31 +8,24 @@ import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.DataTracker.Builder;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
-import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
+import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.predicate.entity.EntityPredicates;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.Box;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
 
 //doesn't move, disappears when player gets near
-public class DarkGhostEntity extends MobEntity {
+public class DarkGhostEntity extends PathAwareEntity {
     public static final String ARM_ANGLE_KEY = "ArmAngle";
-    public static final TrackedData<Float> ARM_ANGLE = DataTracker.registerData(DarkGhostEntity.class,
-            TrackedDataHandlerRegistry.FLOAT);
+    public static final TrackedData<Float> ARM_ANGLE = DataTracker.registerData(DarkGhostEntity.class, TrackedDataHandlerRegistry.FLOAT);
 
     public DarkGhostEntity(EntityType<? extends DarkGhostEntity> entityType, World world) {
         super(entityType, world);
     }
 
     @Override
-    public EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason,
-            EntityData entityData) {
+    public EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, EntityData entityData) {
         getDataTracker().set(ARM_ANGLE, world.getRandom().nextFloat() * 2 * MathHelper.PI);
         return super.initialize(world, difficulty, spawnReason, entityData);
     }
@@ -47,34 +37,9 @@ public class DarkGhostEntity extends MobEntity {
     }
 
     @Override
-    public void tick() {
-        super.tick();
-        if (!(getWorld() instanceof ServerWorld world)) {
-            return;
-        }
-        if (!world.getRegistryKey().getValue().equals(ElementalDimensions.DARK_ID)) {
-            return;
-        }
-        //chance of disappearing each tick if a player is nearby and none have an active radar
-        //should be high if close by, and decrease to 0 at max distance
-        List<PlayerEntity> players = world.getEntitiesByClass(PlayerEntity.class, new Box(getBlockPos()).expand(32), EntityPredicates.EXCEPT_SPECTATOR);
-        //check for active radars
-        ItemStack stack = new ItemStack(ElementalItems.RADAR);
-        for (PlayerEntity player : players) {
-            float cooldown = player.getItemCooldownManager().getCooldownProgress(stack, 0);
-            //5 * 0.5 seconds at 20 blocks/tick means 50 blocks traveled, which is should be sufficiently far for the ghost to be out of the radar pulse
-            if (cooldown >= 0.5) {
-                return;
-            }
-        }
-        for (PlayerEntity player : players) {
-            float dist = player.distanceTo(this);
-            float vanishChance = 0.1f / dist;
-            if (getRandom().nextFloat() < vanishChance) {
-                remove(RemovalReason.KILLED);
-                return;
-            }
-        }
+    protected void initGoals() {
+        super.initGoals();
+        goalSelector.add(2, new DisappearGoal(this));
     }
 
     @Override
---
+++ src/main/java/juniper/elemental/entities/goals/DisappearGoal.java
@@ -0,0 +1,51 @@
+package juniper.elemental.entities.goals;
+
+import java.util.List;
+
+import juniper.elemental.init.ElementalDimensions;
+import juniper.elemental.init.ElementalItems;
+import net.minecraft.entity.Entity.RemovalReason;
+import net.minecraft.entity.ai.goal.Goal;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.predicate.entity.EntityPredicates;
+import net.minecraft.util.math.Box;
+
+public class DisappearGoal extends Goal {
+    private final MobEntity actor;
+
+    public DisappearGoal(MobEntity actor) {
+        this.actor = actor;
+    }
+
+    @Override
+    public boolean canStart() {
+        return actor.getWorld().getRegistryKey().getValue().equals(ElementalDimensions.DARK_ID);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        //chance of disappearing each tick if a player is nearby and none have an active radar
+        //should be high if close by, and decrease to 0 at max distance
+        List<PlayerEntity> players = actor.getWorld().getEntitiesByClass(PlayerEntity.class, new Box(actor.getBlockPos()).expand(32), EntityPredicates.EXCEPT_SPECTATOR);
+        //check for active radars
+        ItemStack stack = new ItemStack(ElementalItems.RADAR);
+        for (PlayerEntity player : players) {
+            float cooldown = player.getItemCooldownManager().getCooldownProgress(stack, 0);
+            //5 * 0.5 seconds at 20 blocks/tick means 50 blocks traveled, which is should be sufficiently far for the ghost to be out of the radar pulse
+            if (cooldown >= 0.5) {
+                return;
+            }
+        }
+        for (PlayerEntity player : players) {
+            float dist = player.distanceTo(actor);
+            float vanishChance = 0.1f / dist;
+            if (actor.getRandom().nextFloat() < vanishChance) {
+                actor.remove(RemovalReason.KILLED);
+                return;
+            }
+        }
+    }
+}
