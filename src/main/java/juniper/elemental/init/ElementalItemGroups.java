9d57b318f179c68d94b69ffa1770a4f859554c89
jupiter <pengjupiter5@gmail.com>
2025-02-28 22:33:20 -0600

    add translations to most items add icon to item group

--- src/main/java/juniper/elemental/init/ElementalItemGroups.java
+++ src/main/java/juniper/elemental/init/ElementalItemGroups.java
@@ -8,6 +8,7 @@ import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -19,6 +20,7 @@ public class ElementalItemGroups {
     public static final RegistryKey<ItemGroup> ELEMENTAL_ITEM_GROUP_KEY = register("elemental");
     public static final ItemGroup ELEMENTAL_ITEM_GROUP = FabricItemGroup.builder()
             .displayName(Text.translatable("itemGroup." + Elemental.MOD_ID))
+            .icon(() -> new ItemStack(ElementalBlocks.CONDUIT))
             .build();
 
     public static final List<ItemConvertible> ALL_ITEMS = new ArrayList<>();
