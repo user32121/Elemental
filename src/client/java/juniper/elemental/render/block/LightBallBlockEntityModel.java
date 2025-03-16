dfba8494608ebe04878181a4b3ffe30c97c5813e
jupiter <pengjupiter5@gmail.com>
2025-03-16 18:23:14 -0500

    light ball blockentity model

--- src/client/java/juniper/elemental/render/block/LightBallBlockEntityModel.java
+++ src/client/java/juniper/elemental/render/block/LightBallBlockEntityModel.java
@@ -13,7 +13,9 @@ import net.minecraft.util.math.MathHelper;
 
 public class LightBallBlockEntityModel extends Model {
     private static final float PI_4 = MathHelper.PI / 4;
-    private static final float ATAN_R_1_2 = (float) Math.atan(Math.sqrt(0.5f));
+    private static final float PI_6 = MathHelper.PI / 6;
+    private static final float TAN_PI_12 = (float) Math.tan(Math.PI / 12);
+    private static final float ATAN_R2_TP12 = (float) Math.atan(Math.sqrt(2) * TAN_PI_12);
 
     public LightBallBlockEntityModel(ModelPart root) {
         super(root, RenderLayer::getEntitySolid);
@@ -22,9 +24,39 @@ public class LightBallBlockEntityModel extends Model {
     public static TexturedModelData getTexturedModelData() {
         ModelData modelData = new ModelData();
         ModelPartData root = modelData.getRoot();
-        ModelPartData core = root.addChild("core", ModelPartBuilder.create(), ModelTransform.of(0, 0, 0, PI_4, 0, ATAN_R_1_2));
-        // TODO
-        core.addChild("cube_1", ModelPartBuilder.create().uv(0, 0).cuboid(-3, -3, -3, 6, 6, 6), ModelTransform.of(0, 0, 0, 0, 0, 0));
+        ModelPartData core = root.addChild("core", ModelPartBuilder.create(), ModelTransform.NONE);
+
+        int quads = 0;
+        int groups = 0;
+        //xy ring
+        for (int i = 0; i < 12; ++i) {
+            core.addChild("quad_" + ++quads, ModelPartBuilder.create().uv(4, 0).cuboid(-1, 1 / TAN_PI_12, -1, 2, 0, 2), ModelTransform.of(0, 0, 0, 0, 0, i * PI_6));
+        }
+        //yz ring
+        for (int i = 0; i < 12; ++i) {
+            if (i % 6 != 0) {
+                core.addChild("quad_" + ++quads, ModelPartBuilder.create().uv(4, 0).cuboid(-1, 1 / TAN_PI_12, -1, 2, 0, 2), ModelTransform.of(0, 0, 0, i * PI_6, 0, 0));
+            }
+        }
+        //xz ring
+        for (int i = 0; i < 12; ++i) {
+            if (i % 3 != 0) {
+                core.addChild("quad_" + ++quads, ModelPartBuilder.create().uv(4, 0).cuboid(-1, 1 / TAN_PI_12, -1, 2, 0, 2), ModelTransform.of(0, 0, 0, MathHelper.PI / 2, i * PI_6, 0));
+            }
+        }
+        //octants
+        for (int i = 0; i < 8; ++i) {
+            int i1 = (i & 1) == 0 ? 0 : 1;
+            int i2 = (i & 2) == 0 ? -1 : 1;
+            int i3 = (i & 4) == 0 ? -1 : 1;
+            core.addChild("group_" + ++groups, ModelPartBuilder.create(), ModelTransform.of(0, 0, 0, i3 * 2 * ATAN_R2_TP12, i2 * PI_4, i1 * MathHelper.PI)).addChild("quad_" + ++quads,
+                    ModelPartBuilder.create().uv(4, 0).cuboid(-1, 1 / TAN_PI_12, -1, 2, 0, 2), ModelTransform.of(0, 0, 0, 0, i2 * -PI_4, 0));
+            core.addChild("group_" + ++groups, ModelPartBuilder.create(), ModelTransform.of(0, 0, 0, i2 * PI_4, 0, i1 * MathHelper.PI)).addChild("quad_" + ++quads,
+                    ModelPartBuilder.create().uv(4, 0).cuboid(1 / TAN_PI_12, -1, -1, 0, 2, 2), ModelTransform.of(0, 0, 0, i2 * -PI_4, 0, i3 * 2 * ATAN_R2_TP12));
+            core.addChild("group_" + ++groups, ModelPartBuilder.create(), ModelTransform.of(0, 0, 0, i3 * -2 * ATAN_R2_TP12, i1 * MathHelper.PI, i2 * -PI_4)).addChild("quad_" + ++quads,
+                    ModelPartBuilder.create().uv(4, 0).cuboid(-1, -1, 1 / TAN_PI_12, 2, 2, 0), ModelTransform.of(0, 0, 0, 0, 0, i2 * PI_4));
+        }
+
         return TexturedModelData.of(modelData, 32, 16);
     }
 
