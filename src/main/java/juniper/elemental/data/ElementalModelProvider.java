792b1c8bcb3e0f6bb8660ae9c73170e4497bc33f
jupiter <pengjupiter5@gmail.com>
2025-03-17 13:20:01 -0500

    change textures of light crystals add light ball texture

--- src/main/java/juniper/elemental/data/ElementalModelProvider.java
+++ src/main/java/juniper/elemental/data/ElementalModelProvider.java
@@ -47,6 +47,8 @@ public class ElementalModelProvider extends FabricModelProvider {
         blockStateModelGenerator.registerSimpleCubeAll(ElementalBlocks.DARK_BLOCK);
         blockStateModelGenerator.registerSimpleCubeAll(ElementalBlocks.DARK_PORTAL);
         blockStateModelGenerator.registerBuiltin(ElementalBlocks.LIGHT_CRYSTAL, ElementalBlocks.LIGHT_CRYSTAL).includeWithoutItem(ElementalBlocks.LIGHT_CRYSTAL);
+        blockStateModelGenerator.registerBuiltin(ElementalBlocks.PACKED_LIGHT_CRYSTAL, ElementalBlocks.PACKED_LIGHT_CRYSTAL).includeWithoutItem(ElementalBlocks.PACKED_LIGHT_CRYSTAL);
+        blockStateModelGenerator.registerBuiltin(ElementalBlocks.LIGHT_BALL, ElementalBlocks.LIGHT_BALL).includeWithoutItem(ElementalBlocks.LIGHT_BALL);
     }
 
     private void registerTriAxisBlock(BlockStateModelGenerator bsmg, Block block) {
@@ -151,5 +153,8 @@ public class ElementalModelProvider extends FabricModelProvider {
         itemModelGenerator.register(ElementalItems.FIRE_SHARD, Models.GENERATED);
         itemModelGenerator.register(ElementalItems.RADAR, Models.GENERATED);
         itemModelGenerator.register(ElementalItems.DARK_SHARD, Models.GENERATED);
+        itemModelGenerator.register(ElementalBlocks.LIGHT_CRYSTAL.asItem(), Models.GENERATED);
+        itemModelGenerator.register(ElementalBlocks.PACKED_LIGHT_CRYSTAL.asItem(), Models.GENERATED);
+        itemModelGenerator.register(ElementalBlocks.LIGHT_BALL.asItem(), Models.GENERATED);
     }
 }
