136aff99a4dfff183f8dc0ddcc52c0a2dc127194
jupiter <pengjupiter5@gmail.com>
2025-03-22 11:05:34 -0700

    remove some unused code

--- src/client/java/juniper/elemental/init/ElementalFluidRenderers.java
+++ src/client/java/juniper/elemental/init/ElementalFluidRenderers.java
@@ -3,18 +3,11 @@ package juniper.elemental.init;
 import juniper.elemental.Elemental;
 import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
 import net.fabricmc.fabric.api.client.render.fluid.v1.SimpleFluidRenderHandler;
-import net.minecraft.client.render.model.SpriteAtlasManager;
-import net.minecraft.client.texture.SpriteAtlasTexture;
-import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.util.Identifier;
 
 public class ElementalFluidRenderers {
     public static void init() {
         FluidRenderHandlerRegistry.INSTANCE.register(ElementalFluids.ALKAHEST,
                 new SimpleFluidRenderHandler(Identifier.of(Elemental.MOD_ID, "block/alkahest"), Identifier.of(Elemental.MOD_ID, "block/alkahest")));
-        // ClientSpriteRegistryCallback.event(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE).register((atlasTexture, registry) -> {
-        //     registry.register(new Identifier("tutorial:block/custom_fluid_still"));
-        //     registry.register(new Identifier("tutorial:block/custom_fluid_flowing"));
-        // });
     }
 }
